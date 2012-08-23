afa85923a3dbc4a9a1edf37611b04ad2ce829266
unknown <jgarcia@.padomain.com>
2012-08-23 09:39:21 -0400

    Initial example/app.js using camera to grab image for ocr. Also added readme to repo

--- example/app.js
+++ example/app.js
@@ -1,39 +1,65 @@
 // This is a test harness for your module
-// You should do something interesting in this harness 
-// to test out the module and to provide instructions 
+// You should do something interesting in this harness
+// to test out the module and to provide instructions
 // to users on how to use it by example.
-
 
 // open a single window
 var win = Ti.UI.createWindow({
-	backgroundColor:'white'
+	title : 'Tesseract OCR Sample',
+	backgroundColor : 'white'
 });
 var label = Ti.UI.createLabel();
 win.add(label);
 win.open();
 
 // TODO: write your module tests here
-var tesseract = require('rmtheis.tesseract.tesstwo');
-Ti.API.info("module is => " + tesseract);
+var tesstwo = require('rmtheis.tesstwo');
+Ti.API.info("module is => " + tesstwo);
 
-label.text = tesseract.example();
-
-Ti.API.info("module exampleProp is => " + tesseract.exampleProp);
-tesseract.exampleProp = "This is a test value";
+var ocrText = Ti.UI.createLabel({
+	color : '#000000',
+	//label using localization-ready strings from <app dir>/i18n/en/strings.xml
+	//text : String.format(L('welcome'), 'Titanium'),
+	text : '',
+	height : 'auto',
+	width : 'auto'
+});
+win.add(ocrText);
 
 if (Ti.Platform.name == "android") {
-	var proxy = tesseract.createExample({
-		message: "Creating an example Proxy",
-		backgroundColor: "red",
-		width: 100,
-		height: 100,
-		top: 100,
-		left: 150
+
+	var cameraButton = Ti.UI.createButton({
+		title : 'launch camera'
+	});
+	win.add(cameraButton);
+
+	var cameraConfig = {
+		saveToPhotoGallery : true,
+		success : function(event) {
+			//ocrText.setText(event.media.nativePath);
+			var foundText = tesstwo.ocr( {path:event.media.nativePath,lang:'eng'} );
+			ocrText.setText(foundText);
+		},
+		cancel : function() {
+		},
+		error : function(error) {
+			var a = Titanium.UI.createAlertDialog({
+				title : 'Video'
+			});
+			if (error.code == Titanium.Media.NO_VIDEO) {
+				a.setMessage('Device does not have video recording capabilities');
+			} else {
+				a.setMessage('Unexpected error: ' + error.code);
+			}
+			a.show();
+		},
+		//overlay : overlay,
+		showControls : false,
+		mediaTypes : Titanium.Media.MEDIA_TYPE_PHOTO,
+		autohide : false
+	};
+	cameraButton.addEventListener('click', function(e) {
+		Titanium.Media.showCamera(cameraConfig);
 	});
 
-	proxy.printMessage("Hello world!");
-	proxy.message = "Hi world!.  It's me again.";
-	proxy.printMessage("Hello world!");
-	win.add(proxy);
 }
-
---
+++ readme.md
@@ -0,0 +1,3 @@
+Titanium Module for Android that provides TessTwo (Tesseract) OCR support
+
+Tesseract binaries need to be generated seperately by using (add repo link here) and then added to this project
